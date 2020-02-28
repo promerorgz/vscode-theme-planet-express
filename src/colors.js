c2eaa8d414e9273a4f9971eef74bf5b151702f96
elbgi <elbgi@monsanto.com>
2020-02-28 11:35:13 -0600

    add logo and settings pjso

---
+++ src/colors.js
@@ -0,0 +1,45 @@
+const pxBlue = "#7FF7EC"
+const darkBlue = "#04324B"
+const pxYellow = "#F8FCBE"
+const pxRed = "#A60B1C"
+const pxGreen = "#328A49"
+const lightGreen = "#B0D197"
+const black = "#000000"
+const white = "#ffffff"
+const gray = "#4A4A4A"
+const darkGray = "#0b0b0b"
+
+// characters
+
+const fry = "#EE7E21"
+const leela = "#96318F"
+const bender = "#C5D0DF"
+const professor = "#B4F3D2"
+const zoidberg = "#ED6A59"
+const zappAndkipp = "#890223"
+
+
+export default {
+  theme: {
+    futurama: {
+      pxBlue,
+      darkBlue,
+      pxYellow,
+      pxRed,
+      pxGreen,
+      lightGreen,
+      black,
+      white,
+      gray,
+      darkGray,
+    },
+    characters: {
+      fry,
+      leela,
+      bender,
+      professor,
+      zoidberg,
+      zappAndkipp
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import colors from './colors';
+
+const darkTheme = {
+  type: "dark",
+  colors: {
+    activityBar: {
+      background: "",
+      border: "",
+      dropBackground: "",
+      foreground: "",
+      badge: {
+        background: "",
+        foreground: "",
+      }
+    },
+    badge: {
+      background: "",
+      foreground: ""
+    },
+    button: {
+      background: "",
+      foreground: "",
+      hoverBackground: ""
+    },
+    contrast: {
+      activeBorder: "",
+      border: ""
+    },
+    debug: {
+      exceptionWidget: {
+        background: "",
+        border: ""
+      },
+      toolbar: {
+        background: ""
+      }
+    },
+    //wip object of all settings
+  }
+}
